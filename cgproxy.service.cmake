0543eed8e8a3c96878860dd4199de3dd0bf7831d
Howard Cheung <mail@h-cheung.cf>
2022-04-01 19:42:41 +0800

    Use firewalld. Apply some patches for Fedora. Patches are from https://github.com/sixg0000d-copr/cgproxy.git

--- cgproxy.service.cmake
+++ cgproxy.service.cmake
@@ -1,10 +1,13 @@
 [Unit]
 Description=cgproxy service
+BindsTo=firewalld.service
+After=firewalld.service
 After=network.target network-online.target
 
 [Service]
 Type=simple
 ExecStart=@CMAKE_INSTALL_FULL_BINDIR@/cgproxyd --execsnoop
+Restart=always
 
 [Install]
 WantedBy=multi-user.target
